c93734799db495e65d0160ee0aecd903df6197c6
rahat <rahat.forja@gmail.com>
2017-11-08 14:31:18 -0600

    added base front end app routes

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -1,26 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import {RouterModule, Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './home/home.component'
 import { LoginComponent } from './login/login.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ProfileComponent } from './profile/profile.component';
 import { RegisterComponent } from './register/register.component';
 
+const appRoutes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'profile', component: ProfileComponent},  //TODO add route guard
+  { path: 'dashboard', component: DashboardComponent},  //TODO add route guard
+  { path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: '**', redirectTo: 'home', pathMatch: 'full'},
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
+    HomeComponent,
     LoginComponent,
     NavbarComponent,
     ProfileComponent,
     RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
