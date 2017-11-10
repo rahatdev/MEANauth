78e8d7e0b7767ecf02319c1593d3144a93a96d7f
rahat <rahat.forja@gmail.com>
2017-11-10 05:42:41 -0600

    added login

--- angular-src/src/app/components/login/login.component.ts
+++ angular-src/src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,10 +10,35 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None
 })
 export class LoginComponent implements OnInit {
+  username: String;
+  password: String;
 
-  constructor() { }
+
+  constructor( 
+    private _flashMessage: FlashMessagesService,
+    private _auth: AuthService,
+    private _router: Router
+  ){ }
 
   ngOnInit() {
+  }
+
+  onLoginSubmit(){
+    const user = {
+      username: this.username,
+      password: this.password
+    }
+
+    this._auth.authenticateUser(user).subscribe(data => {
+      //console.log(data);
+      if(data.success){
+        this._auth.storeUserData(data.token, data.user);
+        this._flashMessage.show('You are now logged in.', {cssClass: 'alert-success', timeout: 3000})
+        this._router.navigate(['/profile']);
+      } else {
+        this._flashMessage.show(data.msg, {cssClass: 'alert-danger', timeout: 3000})
+      }
+    })
   }
 
 }
--- angular-src/src/app/services/auth.service.ts
+++ angular-src/src/app/services/auth.service.ts
@@ -9,18 +9,36 @@ export class AuthService {
 
   constructor(private _http: Http) { }
 
-  registerUser(user){
+  registerUser(user) {
     let headers = new Headers();
     headers.append('Content-Type', 'application/json');
-    return this._http.post('http://localhost:3000/users/register', user, {headers: headers})
-    .map(res => res.json());
+    return this._http.post('http://localhost:3000/users/register', user, { headers: headers })
+      .map(res => res.json());
   }
 
-  authenticateUser(user){
+  authenticateUser(user) {
     let headers = new Headers();
     headers.append('Content-Type', 'application/json');
-    return this._http.post('http://localhost:3000/users/authenticate', user, {headers: headers})
-    .map(res => res.json());
+    return this._http.post('http://localhost:3000/users/authenticate', user, { headers: headers })
+      .map(res => res.json());
+  }
+
+  storeUserData(token, user) {
+    if (token != undefined && user != undefined){
+      //Save to local storage
+      localStorage.setItem('id_token', token);
+      localStorage.setItem('user', JSON.stringify(user)) //should certain items be stored this way? userid?
+
+      //Save local variables
+      this.authToken = token;
+      const newUser = {
+        id: user.id,
+        name : user.name,
+        username: user.username,
+        email: user.email
+      }
+      this.user = newUser;
+    } 
   }
 
 }
